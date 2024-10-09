af400b58c3aa9e80cfdaba951c47589f32c85de2
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 11:31:09 +0100

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,27 @@
+import { GoArrowUpRight } from "react-icons/go";
+import Logo from "../assets/Logo.png";
+import Ogun from "../assets/ogun.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="">
+        <img src={Logo} alt="" />
+        <img src={Ogun} alt="" />
+      </div>
+      <div className="">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="">
+        <button className=" flex">
+          Portal <GoArrowUpRight />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
