eca0520f7695b13d2565846adca18384a029a34e
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-12 22:15:46 +0100

    update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,9 @@
 import AboutIntro from "../components/AboutIntro";
-import Pictures from "../components/Pictures";
 
 const About = () => {
   return (
     <div>
       <AboutIntro />
-
-      <Pictures />
     </div>
   );
 };
