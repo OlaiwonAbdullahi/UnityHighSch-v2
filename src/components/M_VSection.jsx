08b8dc32e51a2a2306e64f4325110eb81eda250a
Jaycode01 <josephlamidi01@gmail.com>
2024-10-13 14:11:52 +0100

    Mission and vision component update

--- src/components/M_VSection.jsx
+++ src/components/M_VSection.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 const M_VSection = () => {
   return (
     <div>
-    <div className='flex justify-between'>
-      <div className=''></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='flex justify-around p-7'>
+      <div className='w-1/5 text-center bg-primary font-titalium p-8'>
+<h3 className='font-poppins font-semibold uppercase text-center'>our mission</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
+      </div>
+      <div className='w-1/5 text-center bg-primary font-titalium p-8'>
+<h3 className='font-poppins font-semibold uppercase text-center'>our vision</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
+      </div>
+      <div className='w-1/5 text-center bg-primary font-titalium  p-8'>
+<h3 className='font-poppins font-semibold uppercase text-center'>core value</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
+      </div>
+      <div className='w-1/5 text-center bg-primary font-titalium  p-8'>
+      <h3 className='font-poppins font-semibold uppercase text-center'>curriculum</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
+      </div>
     </div>
     </div>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
 import AboutIntro from "../components/AboutIntro";
+import M_VSection from "../components/M_VSection";
 
 const About = () => {
   return (
     <div>
       <AboutIntro />
+      <br/>
+      <M_VSection />
     </div>
   );
 };
