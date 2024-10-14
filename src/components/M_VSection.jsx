eb96a388b2410e14b0452c4b824b59bdeaf92e47
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-14 19:52:42 +0100

    update M_VSection.jsx

--- src/components/M_VSection.jsx
+++ src/components/M_VSection.jsx
@@ -1,42 +1,36 @@
-import React from "react";
+import borg from "../assets/borg.svg";
+import eye from "../assets/eye.svg";
 
 const M_VSection = () => {
   return (
     <div>
-      <div className="flex justify-around p-7">
-        <div className="w-1/5 text-center bg-primary font-titalium p-8">
-          <div className=""></div>
-          <h3 className="font-poppins font-semibold uppercase text-center">
-            our mission
+      <div className="flex p-7 justify-center gap-7 flex-col md:flex-row">
+        <div className=" text-center bg-primary p-8 basis-1/2 md:w-1/3 w-full flex flex-col gap-3 rounded-md shadow-md">
+          <div className=" flex justify-center">
+            <img src={borg} alt="" />
+          </div>
+          <h3 className="text-2xl font-titalium font-semibold  text-center">
+            Our Mission
           </h3>
-          To educate all students to the highest levels of academic achievement,
-          to enable them to reach and expand their potential, and to prepare
-          them to become productive, responsible, ethical, creative and
-          compassionate members of society.{" "}
+          <p className=" font-poppins">
+            To educate all students to the highest levels of academic
+            achievement, to enable them to reach and expand their potential, and
+            to prepare them to become productive, responsible, ethical, creative
+            and compassionate members of society.{" "}
+          </p>
         </div>
-        <div className="w-1/5 text-center bg-primary font-titalium p-8">
-          <h3 className="font-poppins font-semibold uppercase text-center">
-            our vision
+        <div className="md:w-1/3 w-full text-center bg-primary  p-8 basis-1/2 flex flex-col gap-3 rounded-md shadow-md">
+          <div className=" flex justify-center">
+            <img src={eye} alt="" />
+          </div>
+          <h3 className="font-titalium text-2xl font-semibold  text-center ">
+            Our vision
           </h3>
-          Empowering students to reach their full potential through innovative
-          education, fostering creativity, and nurturing a culture of lifelong
-          learning.
-        </div>
-        <div className="w-1/5 text-center bg-primary font-titalium  p-8">
-          <h3 className="font-poppins font-semibold uppercase text-center">
-            core value
-          </h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus
-          ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse
-          platea dictumst.
-        </div>
-        <div className="w-1/5 text-center bg-primary font-titalium  p-8">
-          <h3 className="font-poppins font-semibold uppercase text-center">
-            curriculum
-          </h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus
-          ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse
-          platea dictumst.
+          <p className=" font-poppins">
+            Empowering students to reach their full potential through innovative
+            education, fostering creativity, and nurturing a culture of lifelong
+            learning.
+          </p>
         </div>
       </div>
     </div>
