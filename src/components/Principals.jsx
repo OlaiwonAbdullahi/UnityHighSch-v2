cd890b3ee6ebbe6daa8a9eb72f073dfe1cb033d3
Jaycode01 <josephlamidi01@gmail.com>
2024-10-14 10:48:40 +0100

    Update About Page - Past and current pricipal details with reponsiveness

---
+++ src/components/Principals.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const Principals = () => {
+  return (
+    <div>
+      <table class="table-auto w-full p-5 text-left font-poppins border">
+  <thead>
+    <tr className='capitalize w-1/2 '>
+      <th>names</th>
+      <th>date-in</th>
+      <th>date-out</th>
+    </tr>
+  </thead>
+  <tbody className='border'>
+    <tr className='border'>
+      <td>Mrs. Martins Later Mrs. Shobande</td>
+      <td>1980</td>
+      <td>1984</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Bakrin</td>
+      <td>1984</td>
+      <td>1987</td>
+    </tr>
+    <tr className='border'>
+      <td>(Mrs) Chief Opajobi.O.Or</td>
+      <td>1987</td>
+      <td>1992</td>
+    </tr>
+    <tr className='border'>
+      <td>(Mrs) Chief Opajobi.O.Or</td>
+      <td>1987</td>
+      <td>1992</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Omotola.A</td>
+      <td>1993</td>
+      <td>1995</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. M.O Ajayi</td>
+      <td>1995</td>
+      <td>1997</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. B.B Lawal</td>
+      <td>1997</td>
+      <td>1998</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. B.O Okutade</td>
+      <td>1998</td>
+      <td>2000</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Sholanke E.O</td>
+      <td>2000</td>
+      <td>2010</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Adeoye M.O</td>
+      <td>2010</td>
+      <td>2011</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Jibowo S.S</td>
+      <td>2011</td>
+      <td>2014</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Sobulo O.O</td>
+      <td>2014</td>
+      <td>2020</td>
+    </tr>
+    <tr className='border'>
+      <td>Mrs. Akinbode I.O</td>
+      <td>2020</td>
+      <td>2020</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Adeoye M.O</td>
+      <td>2020</td>
+      <td>2023</td>
+    </tr>
+    <tr className='border'>
+      <td>Mr. Akinbode T.D</td>
+      <td>2023</td>
+      <td>Present</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default Principals
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,18 @@
 import AboutIntro from "../components/AboutIntro";
 import M_VSection from "../components/M_VSection";
+import Principals from "../components/Principals";
 
 const About = () => {
   return (
     <div>
+      <div>
       <AboutIntro />
+    </div>
       <div className=" w-4/5 flex justify-center items-center mx-auto">
         <M_VSection />
+      </div>
+        <div>
+        <Principals />
       </div>
     </div>
   );
