6d34e5c2a734655396b4e96a34e33b764e45e0c4
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-14 09:32:21 +0100

    update M_VSection.jsx

--- src/components/M_VSection.jsx
+++ src/components/M_VSection.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const M_VSection = () => {
   return (
     <div>
-    <div className='flex justify-around p-7'>
-      <div className='w-1/5 text-center bg-primary font-titalium p-8'>
-<h3 className='font-poppins font-semibold uppercase text-center'>our mission</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
-      </div>
-      <div className='w-1/5 text-center bg-primary font-titalium p-8'>
-<h3 className='font-poppins font-semibold uppercase text-center'>our vision</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
-      </div>
-      <div className='w-1/5 text-center bg-primary font-titalium  p-8'>
-<h3 className='font-poppins font-semibold uppercase text-center'>core value</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
-      </div>
-      <div className='w-1/5 text-center bg-primary font-titalium  p-8'>
-      <h3 className='font-poppins font-semibold uppercase text-center'>curriculum</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse platea dictumst.
+      <div className="flex justify-around p-7">
+        <div className="w-1/5 text-center bg-primary font-titalium p-8">
+          <h3 className="font-poppins font-semibold uppercase text-center">
+            our mission
+          </h3>
+          To educate all students to the highest levels of academic achievement,
+          to enable them to reach and expand their potential, and to prepare
+          them to become productive, responsible, ethical, creative and
+          compassionate members of society.{" "}
+        </div>
+        <div className="w-1/5 text-center bg-primary font-titalium p-8">
+          <h3 className="font-poppins font-semibold uppercase text-center">
+            our vision
+          </h3>
+          Empowering students to reach their full potential through innovative
+          education, fostering creativity, and nurturing a culture of lifelong
+          learning.
+        </div>
+        <div className="w-1/5 text-center bg-primary font-titalium  p-8">
+          <h3 className="font-poppins font-semibold uppercase text-center">
+            core value
+          </h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus
+          ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse
+          platea dictumst.
+        </div>
+        <div className="w-1/5 text-center bg-primary font-titalium  p-8">
+          <h3 className="font-poppins font-semibold uppercase text-center">
+            curriculum
+          </h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lectus
+          ex, dictum eu pretium vel, ullamcorper quis augue. In hac habitasse
+          platea dictumst.
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default M_VSection
+export default M_VSection;
