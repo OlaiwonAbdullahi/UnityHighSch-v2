35320497814f261b9f4d01cc995325f2112dd8cf
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-14 16:10:06 +0100

    update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -6,28 +6,30 @@ const MessageForm = () => {
       <div className="flex p-3 gap-4">
         <div className="relative basis-3/5">
           <div className="relative">
-            <img src={contact} alt="" className="h-full w-full" />
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <img src={contact} alt="" className="h-full w-full rounded-l-2xl" />
+
+            <div className="absolute inset-0 rounded-l-2xl bg-black bg-opacity-70 flex items-center justify-center">
               <h2 className="text-white text-4xl font-semibold">
                 Get in Touch
               </h2>
             </div>
           </div>
         </div>
-        <div className="basis-2/5">
+        <div className="basis-2/5 flex items-center justify-center flex-col">
           <h2 className="text-3xl font-semibold font-titalium text-center">
             Message Us
           </h2>
-          <span className=" font-poppins">Get in Touch with us✨</span>
-          <form action="">
+          <span className=" font-poppins text-center">
+            Get in Touch with us✨
+          </span>
+          <form action="" className="w-full max-w-sm">
             <div className="my-4">
               <label htmlFor="email"></label>
               <input
                 type="email"
                 id="email"
                 placeholder="Email"
-                className="border border-gray-300 rounded p-2 w-full"
+                className="border border-secondary rounded p-2 w-full font-titalium placeholder:font-titalium bg-transparent focus:outline-none"
               />
             </div>
 
@@ -37,7 +39,7 @@ const MessageForm = () => {
                 type="text"
                 id="Name"
                 placeholder="Name"
-                className="border border-gray-300 rounded p-2 w-full"
+                className="border border-secondary rounded p-2 w-full font-titalium placeholder:font-titalium bg-transparent focus:outline-none"
               />
             </div>
 
@@ -45,7 +47,7 @@ const MessageForm = () => {
               name="Message"
               id=""
               placeholder="Your Message"
-              className="border border-gray-300 rounded p-2 w-full h-32"
+              className="border rounded border-secondary p-2 w-full h-32 font-titalium placeholder:font-titalium bg-transparent focus:outline-none"
             ></textarea>
           </form>
         </div>
