1583c3cd28168da5e9d3b8e03d515561610d0eec
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-14 15:34:41 +0100

    update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -5,6 +5,14 @@ const MessageForm = () => {
         <div className="">
           <img src="" alt="" />
         </div>
+        <div className="">
+          <h2>Message Us</h2>
+          <span>Get in Touch with us✨</span>
+          <form action="">
+            <label htmlFor="email"></label>
+            <input type="email" id="email" />
+          </form>
+        </div>
       </div>
     </div>
   );
