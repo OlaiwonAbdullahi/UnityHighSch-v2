f77b59d9206aff594e2a29a87920c9d838f379f9
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-16 21:38:10 +0100

    update Demography.jsx

--- src/components/Demography.jsx
+++ src/components/Demography.jsx
@@ -7,7 +7,7 @@ const Demography = () => {
         {" "}
         Our Student <span className=" text-secondary">Demography</span>{" "}
       </h2>
-      <div className="flex justify-around gap-6 w-full md:w-3/4">
+      <div className="flex flex-col md:flex-row justify-around gap-6 w-full md:w-3/4">
         <div className="border border-primary basis-1/3 rounded-md p-4 flex flex-col items-center justify-center shadow-sm bg-white">
           <h2 className="text-3xl font-bold font-titalium">1387</h2>
           <span className="text-lg font-poppins">Total</span>
